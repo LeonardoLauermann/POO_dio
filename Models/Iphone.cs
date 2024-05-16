3b5014a78adf6e66667c0aa9f253b71f7a04e936
Leonardo <leolds010101@gmail.com>
2024-05-16 19:28:49 -0300

    solucao

---
+++ Models/Iphone.cs
@@ -0,0 +1,15 @@
+﻿namespace POO.Models
+{
+    public class Iphone : Smartphone
+    {
+        public Iphone(string numero, string modelo, string imei, int memoria) : base(numero, modelo, imei, memoria)
+        {
+        }
+
+        public override void InstalarAplicativo(string nomeApp)
+        {
+            Console.WriteLine(Numero);
+            Console.WriteLine(nomeApp);
+        }
+    }
+}
---
+++ Program.cs
@@ -0,0 +1,8 @@
+﻿using POO.Models;
+
+Nokia nokia = new Nokia("123123", "testeNokia", "testeNokia", 1);
+nokia.InstalarAplicativo("Ifood Nokia");
+
+Iphone iphone = new Iphone("1241231412", "testeIphone", "testeIphone", 1);
+iphone.InstalarAplicativo("Ifood Iphone");
+
